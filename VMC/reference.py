dca46f36471e39132c653d2dd64d44581eb893ac
Lucas K. Wagner <lucas.wagner@gmail.com>
2017-05-08 16:12:27 -0500

    Updated VMC reference. Now have a working evaluation of a Slater determinant.

--- VMC/reference.py
+++ VMC/reference.py
@@ -10,43 +10,72 @@ import wavefunction
 #------------------------------------
 
 class Hamiltonian:
-  def __init__(self):
+  def __init__(self,Z=2):
+    self.Z=Z
     pass
-  def V(self):
-    pass
+  def EN(self,x):
+    r=np.sqrt(np.sum(x**2,axis=1))
+    return np.sum(-self.Z/r,axis=0)
+  def EE(self,x):
+    ree=np.sqrt(np.sum((x[0,:,:]-x[1,:,:])**2,axis=0))
+    return 1/ree
+  def V(self,x):
+    return self.EN(x)#+self.EE(x)
 #------------------------------------
 
   
-def MetropolisSample(x,wf):
+def MetropolisSample(x,wf,tau=0.01,nstep=1000):
   """
   Input variables:
-    x: a 3D numpy array with indices (configuration,electron,[x,y,z] ) 
+    x: a 3D numpy array with indices (electron,[x,y,z],configuration ) 
     wf: a Wavefunction object with value(), gradient(), and laplacian()
   Returns: 
     xnew: A 3D numpy array of configurations the same shape as x, distributed according to psi^2
+    acceptance ratio: 
   """
-  pass
+  xnew=x.copy()
+  xold=x.copy()
+  wfold=wf.value(xold)
+  acceptance=0.0
+  nconf=x.shape[2]
+  for s in range(nstep):
+    xnew=xold+tau*np.random.standard_normal(xold.shape)
+    wfnew=wf.value(xnew)
+    acc=wfnew**2/wfold**2 + np.random.random_sample(nconf) > 1.0
+    #print(acc)
+    xold[:,:,acc]=xnew[:,:,acc]
+    wfold[acc]=wfnew[acc]
+    acceptance+=np.mean(acc)/nstep
+    
+
+  return xold,acceptance
 
 #------------------------------------
 def LocalEnergy(x,wf,H):
-  return -0.5*wf.laplacian(x)+H.V(x)
+  return -0.5*np.sum(wf.laplacian(x),axis=0)+H.V(x)
 
-
-#How this might be used
 
 if __name__=="__main__":
-  nconfig=100
+  nconfig=10000
   ndim=3
   nelec=2
-  wf=SlaterWf(alpha=1.0)
-  H=Hamiltonian()
-  #start all electrons at zero
-  x0=np.zeros((nconfig,nelec,ndim)) 
-  xsample=MetropolisSample(x,wf)
-  eloc=LocalEnergy(xsample,wf,H)
-  eavg=np.mean(eloc)
-  eerr=np.std(eloc)/np.sqrt(nconfig)
-  print("eavg",eavg,"+/-",eerr)
+  nstep=1000
+  wf=wavefunction.ExponentSlaterWF(alpha=1.0)
+  H=Hamiltonian(Z=1)
+  xsample=np.random.randn(nelec,ndim,nconfig)
+  xsample,acc=MetropolisSample(xsample,wf,tau=0.5,nstep=nstep)
+  ke=-0.5*np.sum(wf.laplacian(xsample),axis=0)
+  vion=H.EN(xsample)
+  vee=H.EE(xsample)
+  eloc=ke+vion+vee
+  print("Cycle finished; acceptance",acc)
+  refs=[1.0,-2.0,0.0,-1.0]
+  for nm,quant,ref in zip(['kinetic','Electron-nucleus','Electron-electron','total'],[ke,vion,vee,eloc],refs):
+    avg=np.mean(quant)
+    err=np.std(quant)/np.sqrt(nconfig)
+    print(nm,avg,"+/-",err, "reference",ref)
+
+
   
 
   
--- VMC/wavefunction.py
+++ VMC/wavefunction.py
@@ -1,9 +1,53 @@
-
+import numpy as np
 
 #The wave function object. 
 #This takes in a position and returns 
 #either the value of the wave function or derivatives.
 class Wavefunction:
+  """ Positions are three index arrays as follows:
+    [particle,dimension,configuration]
+  """
+  def __init__(self):
+    pass
+#-------------------------
+  def value(self,x):
+    pass
+#-------------------------
+  def gradient(self,x):
+    """ Return grad psi/psi as a 3D array with dimensions
+    [particle, dimension, configuration]
+    """
+    pass
+#-------------------------
+  def laplacian(self,x):
+    pass
+#-------------------------
+
+########################################
+
+class ExponentSlaterWF:
+  def __init__(self,alpha=1):
+    self.alpha=alpha
+    pass
+#-------------------------
+  def value(self,x):
+    r=np.sqrt(np.sum(x**2,axis=1))
+    return np.exp(-self.alpha*r[0,:])*np.exp(-self.alpha*r[1,:])
+#-------------------------
+  def gradient(self,x):
+    r=np.sqrt(np.sum(x**2,axis=1))
+    return -self.alpha*x/r[:,np.newaxis,:]
+#-------------------------
+  def laplacian(self,x):
+    r=np.sqrt(np.sum(x**2,axis=1))
+    xoverr=x/r[:,np.newaxis,:]
+    return np.sum(self.alpha**2 * xoverr**2 
+        -self.alpha * (xoverr - xoverr**3)/x, 
+        axis=1)
+#-------------------------
+
+########################################
+class JastrowWF:
   def __init__(self):
     pass
 #-------------------------
@@ -18,38 +62,7 @@ class Wavefunction:
 #-------------------------
 
 ########################################
-
-class SlaterWf:
-  def __init__(self):
-    pass
-#-------------------------
-  def value(self,x):
-    pass
-#-------------------------
-  def gradient(self,x):
-    pass
-#-------------------------
-  def laplacian(self,x):
-    pass
-#-------------------------
-
-########################################
-class JastrowWf:
-  def __init__(self):
-    pass
-#-------------------------
-  def value(self,x):
-    pass
-#-------------------------
-  def gradient(self,x):
-    pass
-#-------------------------
-  def laplacian(self,x):
-    pass
-#-------------------------
-
-########################################
-class MultiplyWf:
+class MultiplyWF:
   def __init__(self,wf1,wf2):
     pass
 #-------------------------
@@ -63,5 +76,59 @@ class MultiplyWf:
     pass
 #-------------------------
 
+########################################
 
+def derivativeTest(testpos,wf,delta=1e-4):
+  wf0=wf.value(testpos)
+  grad0=wf.gradient(testpos)
+  npart=testpos.shape[0]
+  ndim=testpos.shape[1]
+  grad_numeric=np.zeros(grad0.shape)
+  for p in range(npart):
+    for d in range(ndim):
+      shift=np.zeros(testpos.shape)
+      shift[p,d,:]+=delta
+      wfval=wf.value(testpos+shift)
+      grad_numeric[p,d,:]=(wfval-wf0)/(wf0*delta)
+  
+  return np.sqrt(np.sum((grad_numeric-grad0)**2)/(npart*testpos.shape[2]*ndim))
 
+########################################
+    
+def laplacianTest(testpos,wf,delta=1e-5):
+  wf0=wf.value(testpos)
+  lap0=wf.laplacian(testpos)
+  npart=testpos.shape[0]
+  ndim=testpos.shape[1]
+  
+  lap_numeric=np.zeros(lap0.shape)
+  for p in range(npart):
+    for d in range(ndim):
+      shift=np.zeros(testpos.shape)
+      shift[p,d,:]+=delta      
+      wf_plus=wf.value(testpos+shift)
+      shift[p,d,:]-=2*delta      
+      wf_minus=wf.value(testpos+shift)
+      # Here we use the value so that the laplacian and gradient tests
+      # are independent
+      lap_numeric[p,:]+=(wf_plus+wf_minus-2*wf0)/(wf0*delta**2)
+  
+  return np.sqrt(np.sum((lap_numeric-lap0)**2)/(npart*testpos.shape[2]))
+########################################
+
+if __name__=="__main__":
+  wf=ExponentSlaterWF(0.5)
+  testpos=np.random.randn(2,3,5)
+  df={'delta':[],
+      'rms derivative':[],
+      'rms laplacian':[]
+      }
+  for delta in [1e-2,1e-3,1e-4,1e-5,1e-6]:
+    df['delta'].append(delta)
+    df['rms derivative'].append(derivativeTest(testpos,wf,delta))
+    df['rms laplacian'].append(laplacianTest(testpos,wf,delta))
+
+  import pandas as pd
+  print(pd.DataFrame(df))
+
+  
