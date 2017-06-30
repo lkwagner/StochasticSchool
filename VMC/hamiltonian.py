4cd6964e8b31c0da6536a85c5a0908c27b662027
Brian Busemeyer <bbusemeyer@gmail.com>
2017-06-30 16:46:57 -0500

    Forgot to actually remove the solutions.

--- VMC/hamiltonian.py
+++ VMC/hamiltonian.py
@@ -7,12 +7,12 @@ class Hamiltonian:
     self.Z=Z
   def pot_en(self,pos):
     """ electron-nuclear potential of configurations 'pos' """
-    r=np.sqrt(np.sum(pos**2,axis=1))
-    return np.sum(-self.Z/r,axis=0)
+    pass
+    return np.zeros(pos.shape[2])
   def pot_ee(self,pos):
     """ electron-electron potential of configurations 'pos' """
-    ree=np.sqrt(np.sum((pos[0,:,:]-pos[1,:,:])**2,axis=0))
-    return 1/ree
+    pass 
+    return np.zeros(pos.shape[2])
   def pot(self,pos):
     """ potential energy of configuations 'pos' """
     return self.pot_en(pos)+self.pot_ee(pos)
