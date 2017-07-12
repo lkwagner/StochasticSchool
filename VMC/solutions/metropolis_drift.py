30ae4aff44afbc4e269d0dafecdb438e8af92608
Brian Busemeyer <bbusemeyer@gmail.com>
2017-07-11 22:18:51 -0600

    Completed drift instructions.

--- VMC/solutions/metropolis_drift.py
+++ VMC/solutions/metropolis_drift.py
@@ -18,12 +18,12 @@ def drift_vector(pos,wf,tau,scaled=False):
         dvec   *= vscale[:,np.newaxis,:]
     return dvec
 
-def drift_prob(posold,posnew,gauss_move_old,driftnew,tau):
+def drift_prob(poscur,posnew,gauss_move_cur,driftnew,tau):
     """ return the ratio of forward and backfward move probabilities for rejection algorith,
     Input:
-      posold: electron positions before move (nelec,ndim,nconf) 
+      poscur: electron positions before move (nelec,ndim,nconf) 
       posnew: electron positions after  move (nelec,ndim,nconf)
-      gauss_move_old: randn() numbers for forward move
+      gauss_move_cur: randn() numbers for forward move
       driftnew: drift vector at posnew 
       tau: time step
     Return:
@@ -31,14 +31,14 @@ def drift_prob(posold,posnew,gauss_move_old,driftnew,tau):
       """
 
     # randn numbers needed for backward move
-    gauss_move_new = (posold-posnew-tau*driftnew)/np.sqrt(tau)
+    gauss_move_new = (poscur-posnew-tau*driftnew)/np.sqrt(tau)
     # assume the following drift-diffusion move
-    #assert np.allclose( posold,posnew+np.sqrt(tau)*gauss_move_new+tau*driftnew ) 
+    #assert np.allclose( poscur,posnew+np.sqrt(tau)*gauss_move_new+tau*driftnew ) 
 
     # calculate move probabilities
-    gauss_old_sq = np.sum( np.sum(gauss_move_old**2.,axis=1) ,axis=0)
+    gauss_cur_sq = np.sum( np.sum(gauss_move_cur**2.,axis=1) ,axis=0)
     gauss_new_sq = np.sum( np.sum(gauss_move_new**2.,axis=1), axis=0)
-    forward_green  = np.exp(-gauss_old_sq/2.)
+    forward_green  = np.exp(-gauss_cur_sq/2.)
     backward_green = np.exp(-gauss_new_sq/2.)
 
     ratio = backward_green/forward_green
@@ -57,34 +57,34 @@ def metropolis_sample_biased(pos,wf,tau=0.01,nstep=1000):
 
   # initialize
   posnew = pos.copy()
-  posold = pos.copy()
-  wfold  = wf.value(posold)
-  driftold = drift_vector(posold,wf,tau=tau,scaled=True)
+  poscur = pos.copy()
+  wfcur  = wf.value(poscur)
+  driftcur = drift_vector(poscur,wf,tau=tau,scaled=True)
   acceptance=0.0
   nconf=pos.shape[2]
   for istep in range(nstep):
 
     # propose a move
-    gauss_move_old = np.random.randn(*posold.shape)
-    posnew=posold+np.sqrt(tau)*gauss_move_old
-    posnew += tau*driftold
+    gauss_move_cur = np.random.randn(*poscur.shape)
+    posnew=poscur+np.sqrt(tau)*gauss_move_cur
+    posnew += tau*driftcur
 
     wfnew=wf.value(posnew)
 
     # calculate Metropolis-Rosenbluth-Teller acceptance probability
     #  VMC uses rejection to sample psi_sq by maintaining detailed balance
-    prob = wfnew**2/wfold**2 # for reversible moves
+    prob = wfnew**2/wfcur**2 # for reversible moves
     driftnew = drift_vector(posnew,wf,tau=tau,scaled=True)
-    prob *= drift_prob(posold,posnew,gauss_move_old,driftnew,tau)
+    prob *= drift_prob(poscur,posnew,gauss_move_cur,driftnew,tau)
 
     # get indices of accepted moves
     acc_idx = (prob + np.random.random_sample(nconf) > 1.0)
 
     # update stale stored values for accepted configurations
-    posold[:,:,acc_idx] = posnew[:,:,acc_idx]
-    driftold[:,:,acc_idx] = driftnew[:,:,acc_idx]
-    wfold[acc_idx] = wfnew[acc_idx]
+    poscur[:,:,acc_idx] = posnew[:,:,acc_idx]
+    driftcur[:,:,acc_idx] = driftnew[:,:,acc_idx]
+    wfcur[acc_idx] = wfnew[acc_idx]
     acceptance += np.mean(acc_idx)/nstep
 
-  return posold,acceptance
+  return poscur,acceptance
 
